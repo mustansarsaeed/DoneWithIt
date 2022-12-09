9bbf7f2695d65e97964cfd050511204cf401ccc9
Mustansar Saeed <m.mustansar.saeed@gmail.com>
2022-12-09 15:26:13 +0500

    - Created custom Input component

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextInput, Text } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 
 import Icon from "./app/components/Icon";
 import Screen from "./app/components/Screen";
@@ -6,5 +8,10 @@ import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
